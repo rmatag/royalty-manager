62a18e36956ab41b70f2ba16dd298f37f04d3bfc
Raul Mata <rmata@theworkshop.com>
2017-07-26 10:09:58 +0200

    Adding CORS Filters

--- royalty-manager-api/src/main/java/com/royalty/config/WebSecurityConfiguration.java
+++ royalty-manager-api/src/main/java/com/royalty/config/WebSecurityConfiguration.java
@@ -1,42 +1,41 @@
 package com.royalty.config;
 
-import jersey.repackaged.com.google.common.collect.ImmutableList;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.security.web.access.channel.ChannelProcessingFilter;
+import org.springframework.web.cors.CorsUtils;
 
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Arrays;
 
-@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+    private static final String[] ALLOWED_USERS = { "user" };
 
-    private static final String[] ALLOWED_USERS = {"user1", "user2"};
-    private static final String PASSWORD = "password";
+    private static final String PASSWORD = "user";
 
     @Bean
     public UserDetailsService userDetailsService() {
         return username -> {
 
-            if(Arrays.asList(ALLOWED_USERS).contains(username)) {
+            if (Arrays.asList(ALLOWED_USERS).contains(username)) {
                 return new User(username, PASSWORD, true, true, true, true,
                         AuthorityUtils.createAuthorityList("ROLE_USER"));
             } else {
@@ -45,33 +44,41 @@ class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
         };
     }
 
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService());
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-                .httpBasic().and()
+        http
                 .authorizeRequests()
+                .requestMatchers(CorsUtils::isCorsRequest).permitAll()
                 .anyRequest().authenticated()
-                .and().anonymous().disable()
-                .exceptionHandling().authenticationEntryPoint(new BasicAuthenticationEntryPoint() {
-            @Override
-            public void commence(final HttpServletRequest request, final HttpServletResponse response, final AuthenticationException authException)
-                    throws IOException, ServletException {
-                if(HttpMethod.OPTIONS.matches(request.getMethod())){
-                    response.setStatus(HttpServletResponse.SC_OK);
-                    response.setHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, request.getHeader(HttpHeaders.ORIGIN));
-                    response.setHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_HEADERS, request.getHeader(HttpHeaders.ACCESS_CONTROL_REQUEST_HEADERS));
-                    response.setHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_METHODS, request.getHeader(HttpHeaders.ACCESS_CONTROL_REQUEST_METHOD));
-                    response.setHeader(HttpHeaders.ACCESS_CONTROL_ALLOW_CREDENTIALS, "true");
-                }else{
-                    response.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException.getMessage());
-                }
-            }
-        });
+                .and().httpBasic()
+                .and().addFilterBefore(new WebSecurityCorsFilter(), ChannelProcessingFilter.class);
     }
 
+    public class WebSecurityCorsFilter implements Filter {
+        @Override
+        public void init(FilterConfig filterConfig) throws ServletException {
+        }
+
+        @Override
+        public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+                throws IOException, ServletException {
+            HttpServletResponse res = (HttpServletResponse) response;
+            res.setHeader("Access-Control-Allow-Origin", "*");
+            res.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
+            res.setHeader("Access-Control-Max-Age", "3600");
+            res.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, Accept, x-requested-with, Cache-Control");
+            chain.doFilter(request, res);
+        }
+
+        @Override
+        public void destroy() {
+        }
+
+    }
 }
+
